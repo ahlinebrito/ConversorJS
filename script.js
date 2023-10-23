7da1a50d2da0c593cc26566feb57f777c50ef0fc
ahlinebrito <120393377+ahlinebrito@users.noreply.github.com>
2023-10-23 20:53:34 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+document.getElementById("toCelsius").addEventListener("click", function () {
+    var inputTemperature = document.getElementById("temperature").value;
+    var celsius = (inputTemperature - 32) * 5/9;
+    document.getElementById("result").innerHTML = inputTemperature + "°F é igual a " + celsius.toFixed(2) + "°C.";
+});
+
+document.getElementById("toFahrenheit").addEventListener("click", function () {
+    var inputTemperature = document.getElementById("temperature").value;
+    var fahrenheit = (inputTemperature * 9/5) + 32;
+    document.getElementById("result").innerHTML = inputTemperature + "°C é igual a " + fahrenheit.toFixed(2) + "°F.";
+});
